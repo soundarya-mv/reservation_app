3250033f54863c3d91afa4cc2d7921eacb753dff
soundarya-mv <soundarya1187@gmail.com>
2023-08-04 09:42:04 +1000

    Adding seed file for populating currencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+	Currency.create(name: "AUD")
+	Currency.create(name: "INR")
+	Currency.create(name: "USD")
+	Currency.create(name: "GBP")
